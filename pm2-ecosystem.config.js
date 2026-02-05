13160cfa05ff4cb3d5e68ed6920270d6c58060ea
walisongomes <wgwalisongomes@gmail.com>
2026-02-05 02:55:33 -0300

    fix bug

--- pm2-ecosystem.config.js
+++ pm2-ecosystem.config.js
@@ -1,13 +1,17 @@
 //pm2 process
 module.exports = {
-    apps: [{
-        name: 'bx-whatsapp',
-        script: 'yarn start',
-        env_production: {
-            NODE_ENV: 'production',
-        },
-        env_development: {
-            NODE_ENV: 'development',
-        },
-    }],
+  apps: [
+    {
+      name: 'bx-whatsapp',
+      script: 'app.js', // ou o entrypoint real
+      interpreter: 'node',
+      node_args: '--enable-source-maps',
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      env_development: {
+        NODE_ENV: 'development'
+      }
+    }
+  ]
 }
